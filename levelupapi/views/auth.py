16299003da9600a1c3efc8d7722babd1de02a431
Tingting Duan <duan.tingting@yahoo.com>
2021-12-18 15:17:53 -0500

    chapter 5

--- levelupapi/views/auth.py
+++ levelupapi/views/auth.py
@@ -1,11 +1,14 @@
 from django.contrib.auth import authenticate
-from django.contrib.auth.models import User
+# from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model
 from rest_framework.authtoken.models import Token
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 
 from levelupapi.models import Gamer
+
+User = get_user_model()
 
 @api_view(['POST'])
 @permission_classes([AllowAny])
