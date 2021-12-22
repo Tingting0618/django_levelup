744adb564e22b985cc35144dcd53113fa1fae4b1
Tingting Duan <duan.tingting@yahoo.com>
2021-12-21 22:26:06 -0500

    chapter 9

--- levelupapi/models/event.py
+++ levelupapi/models/event.py
@@ -1,10 +1,16 @@
 from django.db import models
+from levelupapi.models.game import Game
 from levelupapi.models.gamer import Gamer
 
 
 class Event(models.Model):
-    creator = models.ForeignKey(
-        Gamer, verbose_name="creator", on_delete=models.SET_NULL, null=True)
-    desc = models.CharField(max_length=50)
-    time = models.DateTimeField(("%Y-%m-%d %H:%M:%S"), auto_now=False, auto_now_add=True)
-                               
+    organizer = models.ForeignKey(
+        Gamer, verbose_name="organizer", on_delete=models.SET_NULL, null=True)
+    description = models.CharField(max_length=50)
+
+    game = models.ForeignKey(
+        Game, verbose_name="organizer", on_delete=models.SET_NULL, null=True)
+    date_time = models.DateTimeField(null=True)
+    date = models.DateField(null=True)
+    time = models.TimeField(null=True)
+
--- levelupapi/models/game.py
+++ levelupapi/models/game.py
@@ -1,11 +1,12 @@
 from django.db import models
-from levelupapi.models.event import Event
+from levelupapi.models.gamer import Gamer
 from levelupapi.models.gametype import GameType
 
 
 class Game(models.Model):
-    name = models.CharField(max_length=50)
-    event = models.ForeignKey(
-        Event, verbose_name="event", on_delete=models.SET_NULL, null=True)
-    type = models.ForeignKey(GameType, verbose_name="game_type", on_delete=models.SET_NULL, null=True)
-                               
+    title = models.CharField(max_length=50,null=True)
+    maker = models.CharField(max_length=50,null=True)
+    game_type = models.ForeignKey(GameType, on_delete=models.SET_NULL, null=True)
+    number_of_players = models.IntegerField(null=True)
+    skill_level = models.IntegerField(null=True)
+    gamer = models.ForeignKey(Gamer,on_delete=models.SET_NULL, null=True,related_name='games')
